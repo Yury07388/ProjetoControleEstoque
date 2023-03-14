e843317aa6453e2cb0b95081384a1973d59e8cbe
yury <yury50716@gmail.com>
2023-03-14 20:58:16 -0300

    Ajustando area da controller

---
+++ Controller/src/main/java/EscritaArquivoCsv.java
@@ -0,0 +1,5 @@
+public class EscritaArquivoCsv {
+    public void PegarListaObjetoMandaParaCsv(){
+
+    }
+}
---
+++ Controller/src/main/java/ListaDeProdutos.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaDeProdutos {
+
+    public void Estoque(){
+        List<Object>listaEstoque = new ArrayList<>();
+        listaEstoque.add("PRODUTO X");
+        System.out.println(listaEstoque);
+    }
+}
